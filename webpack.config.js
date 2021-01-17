98913cad06094cebb430c53cd38a3410e331fe37
Mona Lisa <joannagrosshans2@gmail.com>
2021-01-17 14:34:32 -0600

    webpack built

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = {
 // add plugins to direct webpack on what to do
 // even though jquery package is added, we need to write in plugin
@@ -8,13 +9,52 @@ plugins: [
         $: "jquery",
         jQuery: "jquery"
     }),
+    new webpack.ProvidePlugin({
+        $: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+        // the report outputs to an HTML called report.html in the dist folder
+        // can set to disable to temp. stop reporting and opening of report 
+        analyzerMode: "static",
+
+    })
 ],
 // root of the bundle and beginning of dependency graph, gives relative path of code
-entry: './assets/js/script.js',
+entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+},
 //  takes bundles code and puts in specified folder
 output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    path: __dirname + "/dist",
+    // name from key name from value pair in entry object
+    filename: "[name].bundle.js"
+},
+module: {
+    rules: [
+        {
+            // processes any image file with .jpg 
+            test: /\.jpg$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name (file) {
+                            return "[path][name].[ext]"
+                        },
+                        publicPath: function(url) {
+                            return url.replace("../", "/assets/")
+                        }
+                    }
+                },
+                {
+                loader: 'image-webpack-loader'
+                }
+            ]
+        }
+    ]
 },
 // the mode we want the webpack to run in, production is default
 mode: 'development'
